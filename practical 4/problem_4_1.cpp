ab9e0e69d3e68959828265fa3d55b6f153be1670
savaliyariya03 <savaliyariya03@gmail.com>
2026-09-21 12:50:42 +0530

    Update problem_4_1.cpp

--- practical 4/problem_4_1.cpp
+++ practical 4/problem_4_1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-
-struct Node {
+struct Node
+{
     int data;
     struct Node* next;
 };
-
-int main() {
+int main()
+{
     int choice;
     struct Node* Node1 = (struct Node*)malloc(sizeof(struct Node));
     struct Node* Node2 = (struct Node*)malloc(sizeof(struct Node));
@@ -23,65 +23,63 @@ int main() {
 
     struct Node* head = Node1;
 
-    cout << "Enter your choice:\n";
-    cout << "1. Insert patient at front\n";
-    cout << "2. Insert patient at end\n";
-    cout << "3. Insert patient at specific position\n";
+    cout<<"Enter your choice:\n";
+    cout<<"1. Insert patient at front\n";
+    cout<<"2. Insert patient at end\n";
+    cout<<"3. Insert patient at specific position\n";
     cout<<"4.exit\n";
     do{
-    cout << "Enter: ";
-    cin >> choice;
-    switch (choice)
+    cout<<"Enter your choice: ";
+    cin>>choice;
+    switch(choice)
     {
         case 1:
         {
             struct Node* NewNode = (struct Node*)malloc(sizeof(struct Node));
-            cout << "Enter patient ID: ";
-            cin >> NewNode->data;
+            cout<<"Enter patient ID: ";
+            cin>>NewNode->data;
             NewNode->next = head;
             head = NewNode;
-            cout << "\nAfter inserting, Linked List = ";
+            cout<<"\nAfter inserting, Linked List = ";
             struct Node* travel = head;
-            while (travel != nullptr)
+            while(travel != nullptr)
             {
-                cout << travel->data << " -> ";
+                cout<<travel->data<< " -> ";
                 travel = travel->next;
             }
-            cout << "NULL\n";
+            cout<<"NULL\n";
             break;
         }
-
         case 2: {
             struct Node* NewNode = (struct Node*)malloc(sizeof(struct Node));
-            cout << "Enter patient ID: ";
-            cin >> NewNode->data;
+            cout<<"Enter patient ID: ";
+            cin>>NewNode->data;
             NewNode->next = nullptr;
             struct Node* temp = head;
-            while (temp->next != nullptr)
+            while(temp->next != nullptr)
             {
                 temp = temp->next;
             }
             temp->next = NewNode;
-            cout << "\nAfter inserting, Linked List = ";
+            cout<<"\nAfter inserting, Linked List = ";
             struct Node* travel = head;
-            while (travel != nullptr)
+            while(travel != nullptr)
             {
-                cout << travel->data << " -> ";
+                cout<<travel->data<<" -> ";
                 travel = travel->next;
             }
-            cout << "NULL\n";
+            cout<<"NULL\n";
             break;
         }
-
         case 3:
         {
             int pos;
             struct Node* NewNode = (struct Node*)malloc(sizeof(struct Node));
-            cout << "Enter patient ID: ";
-            cin >> NewNode->data;
-            cout << "Enter position (1-based index): ";
-            cin >> pos;
-            if (pos == 1)
+            cout<<"Enter patient ID: ";
+            cin>>NewNode->data;
+            cout<<"Enter position: ";
+            cin>>pos;
+            if(pos == 1)
             {
                 NewNode->next = head;
                 head = NewNode;
@@ -93,7 +91,7 @@ int main() {
             else
             {
                 struct Node* temp = head;
-                for (int i = 1; i < pos - 1; i++)
+                for(int i=1;i<pos-1;i++)
                 {
                     temp = temp->next;
                 }
@@ -102,12 +100,12 @@ int main() {
             }
             cout << "\nAfter inserting, Linked List = ";
             struct Node* travel = head;
-            while (travel != nullptr)
+            while(travel != nullptr)
             {
-                cout << travel->data << " -> ";
+                cout<<travel->data<<" -> ";
                 travel = travel->next;
             }
-            cout << "NULL\n";
+            cout<<"NULL\n";
             break;
         }
         case 4:
@@ -115,7 +113,7 @@ int main() {
             break;
         }
         default:
-            cout << "Invalid choice!\n";
+            cout <<"Invalid choice!\n";
             return 0;
     }
     }while(choice != 4);
